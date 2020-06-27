69f8223d4ae8e28d102bbc3454f4fa5e11b71167
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2020-06-27 22:56:33 +0900

    Modify to MDCCardCollectionCell

--- swift-material-ui-tutorial/ProductCell.swift
+++ swift-material-ui-tutorial/ProductCell.swift
@@ -1,12 +1,19 @@
 import UIKit
+import MaterialComponents
 
-class ProductCell: UICollectionViewCell {
+class ProductCell: MDCCardCollectionCell {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
 
     override func awakeFromNib() {
       super.awakeFromNib()
+
+        self.backgroundColor = .white
+
+        self.cornerRadius = 4.0;
+        self.setBorderWidth(1.0, for:.normal)
+        self.setBorderColor(.lightGray, for: .normal)
     }
 
 }
