149984841374703328a03d4ae879fa8ef12d5849
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2020-06-28 01:58:49 +0900

    Fix colorScheme

--- swift-material-ui-tutorial/ApplicationScheme.swift
+++ swift-material-ui-tutorial/ApplicationScheme.swift
@@ -21,25 +21,29 @@ class ApplicationScheme: NSObject {
   public let colorScheme: MDCSemanticColorScheme = {
     let scheme = MDCSemanticColorScheme(defaults: .material201804)
     scheme.primaryColor =
-            UIColor(red: 252.0/255.0, green: 184.0/255.0, blue: 171.0/255.0, alpha: 1.0)
+            UIColor(red: 36.0/255.0, green: 42.0/255.0, blue: 51.0/255.0, alpha: 1.0)
     scheme.primaryColorVariant =
-            UIColor(red: 68.0/255.0, green: 44.0/255.0, blue: 46.0/255.0, alpha: 1.0)
+            UIColor(red: 101.0/255.0, green: 99.0/255.0, blue: 96.0/255.0, alpha: 1.0)
     scheme.onPrimaryColor =
-            UIColor(red: 68.0/255.0, green: 44.0/255.0, blue: 46.0/255.0, alpha: 1.0)
+            UIColor(red: 255.0/255.0, green: 251.0/255.0, blue: 250.0/255.0, alpha: 1.0)
     scheme.secondaryColor =
-            UIColor(red: 254.0/255.0, green: 234.0/255.0, blue: 230.0/255.0, alpha: 1.0)
+            UIColor(red: 101.0/255.0, green: 180.0/255.0, blue: 81.0/255.0, alpha: 1.0)
     scheme.onSecondaryColor =
-            UIColor(red: 68.0/255.0, green: 44.0/255.0, blue: 46.0/255.0, alpha: 1.0)
+            UIColor(red: 48.0/255.0, green: 96.0/255.0, blue: 219.0/255.0, alpha: 1.0)
+    // FIXME
     scheme.surfaceColor =
             UIColor(red: 255.0/255.0, green: 251.0/255.0, blue: 250.0/255.0, alpha: 1.0)
+    // FIXME
     scheme.onSurfaceColor =
             UIColor(red: 68.0/255.0, green: 44.0/255.0, blue: 46.0/255.0, alpha: 1.0)
+    // FIXME
     scheme.backgroundColor =
             UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+    // FIXME #363F4D
     scheme.onBackgroundColor =
-            UIColor(red: 68.0/255.0, green: 44.0/255.0, blue: 46.0/255.0, alpha: 1.0)
+            UIColor(red: 54.0/255.0, green: 63.0/255.0, blue: 77.0/255.0, alpha: 1.0)
     scheme.errorColor =
-            UIColor(red: 197.0/255.0, green: 3.0/255.0, blue: 43.0/255.0, alpha: 1.0)
+            UIColor(red: 224.0/255.0, green: 84.0/255.0, blue: 53.0/255.0, alpha: 1.0)
     return scheme
   }()
 
